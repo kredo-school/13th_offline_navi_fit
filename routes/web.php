562ed8441df90d42b93cb62bfcb8dc3e0125845b
96f0-KOHEIKAWASAKI <nagao2211@icloud.com>
2025-06-28 14:52:47 +0800

    resolve routes conflict and remove duplicates

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,13 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/', function () {
     if (Auth::check()) {
         return redirect('/home');
     }
 
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
